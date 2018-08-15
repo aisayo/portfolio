85bcbe334dd23bc306488d600599de39a0e421ea
Aysan Isayo <aisayo@icloud.com>
2018-08-14 21:37:46 -0700

    added Navbar

--- portfolio-app/portfolio-app-client/src/components/Header.js
+++ portfolio-app/portfolio-app-client/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import NavBar from './NavBar';
+import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 
 const Header = () => {
     return(
+        <Router>
         <div className>
-           
+           <NavBar />
         </div>
+        </Router>
     )
 }
 
--- portfolio-app/portfolio-app-client/src/components/NavBar.js
+++ portfolio-app/portfolio-app-client/src/components/NavBar.js
@@ -3,10 +3,39 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return(
-        <div>
-            Navbar
-        </div>
+           <div className="navbar">
+
+            <NavLink
+                to="/home"
+                exact
+                activeStyle={{
+                    color: 'purple'
+                }}
+                > Home 
+            </NavLink> |
+
+                <NavLink
+                to="/contact"
+                activeStyle={{
+                    color: 'black'
+                }}
+                > About</NavLink> |
+
+                <NavLink
+                to="/about"
+                activeStyle={{
+                    color: 'black'
+                }}
+                > Skills </NavLink> |
+
+                <NavLink
+                to="/"
+                activeStyle={{
+                    color: 'black'
+                }}
+                > Blog </NavLink>
+            </div>
     )
 }
 
-export default Navbar;
+export default NavBar;
