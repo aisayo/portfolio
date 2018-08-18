451de0a8cec6d74f659e46a615b1449a0cfc263d
Aysan Isayo <aisayo@icloud.com>
2018-08-17 23:26:13 -0700

    added styling to nav links

--- portfolio-app/portfolio-app-client/src/components/resume/Homepage.js
+++ portfolio-app/portfolio-app-client/src/components/resume/Homepage.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Homepage = () => {
     return (
                 <div className="name">
-                    <text>Hi,I'm Aysan Isayo!</text>
+                    <text>Hi,I'm Aysan!</text>
                 </div>
     )
 }
