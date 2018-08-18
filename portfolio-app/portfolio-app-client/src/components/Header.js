ba3790dd892423ebd945c65057a55df5f2d3cdf2
Aysan Isayo <aisayo@icloud.com>
2018-08-18 15:35:47 -0700

    added switch route for about page

--- portfolio-app/portfolio-app-client/src/components/Header.js
+++ portfolio-app/portfolio-app-client/src/components/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 import NavBar from './NavBar';
 import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 
+import Homepage from './resume/Homepage';
+import About from './About';
+
+
 const Header = () => {
     return(
         <Router>
         <div className="header">
             <div className="navbar">
                 <NavBar />
+                <Switch>
+                    <Route path="/about" component={About} />} />
+                  </Switch>
             </div>
         </div>
         </Router>
